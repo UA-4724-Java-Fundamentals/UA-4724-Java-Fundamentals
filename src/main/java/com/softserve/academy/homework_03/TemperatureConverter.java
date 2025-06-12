22558a3821a226b87ec8da67484a918d2f1919ec
Viktoriia Haliuk <haliuk.viktoriia@gmail.com>
2025-06-12 18:33:12 +0300

    updated fields

--- src/main/java/com/softserve/academy/homework_03/TemperatureConverter.java
+++ src/main/java/com/softserve/academy/homework_03/TemperatureConverter.java
@@ -1,9 +1,9 @@
 package com.softserve.academy.homework_03;
 
 public class TemperatureConverter {
-    public static void main(String[] args) {
+    public static double fahrenheitToCelsius(double fahrenheit){
         double tempFahrenheit = 200;
         double tempCelsius = (tempFahrenheit - 32) * (5.0 / 9.0);
-        System.out.println("Temperature in Celsius: " + tempCelsius);
+        return tempCelsius;
     }
 }
