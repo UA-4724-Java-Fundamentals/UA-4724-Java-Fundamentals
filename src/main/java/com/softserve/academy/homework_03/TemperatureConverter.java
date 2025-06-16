3310bfe9c6feb08c303c9a2471349babe4154d69
Viktoriia Haliuk <haliuk.viktoriia@gmail.com>
2025-06-16 16:02:15 +0300

    Deleted redundant file

--- src/main/java/com/softserve/academy/homework_03/TemperatureConverter.java
+++
@@ -1,9 +0,0 @@
-package com.softserve.academy.homework_03;
-
-public class TemperatureConverter {
-    public static double fahrenheitToCelsius(double fahrenheit){
-        double tempFahrenheit = 200;
-        double tempCelsius = (tempFahrenheit - 32) * (5.0 / 9.0);
-        return tempCelsius;
-    }
-}
