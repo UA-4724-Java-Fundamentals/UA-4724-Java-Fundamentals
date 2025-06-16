bbfce3d9348879e1fc203d5bd94062c44a4aea7b
Voloshchenko T <tasiyxa00@gmail.com>
2025-06-16 14:57:06 +0200

    triandle square

---
+++ src/main/java/com/softserve/academy/homework3/AppTriangle.java
@@ -0,0 +1,21 @@
+package com.softserve.academy.homework3;
+
+import java.util.Scanner;
+
+public class AppTriangle {
+
+    public static void main (String ... args) {
+        Scanner sc = new Scanner(System.in);
+        int side1;
+        int side2;
+        int side3;
+        System.out.println("Enter side 1:");
+        side1 = sc.nextInt();
+        System.out.println("Enter side 2:");
+        side2 = sc.nextInt();
+        System.out.println("Enter side 3:");
+        side3 = sc.nextInt();
+        Triangle triangle = new Triangle(side1, side2,side3);
+        System.out.println("Square = " + triangle.getSquare());
+    }
+}
--- src/main/java/com/softserve/academy/homework3/Triangle.java
+++ src/main/java/com/softserve/academy/homework3/Triangle.java
@@ -5,12 +5,43 @@ public class Triangle {
     private int side2;
     private int side3;
 
-    public Triangle(){};
+    public int getSide1() {
+        return side1;
+    }
 
-    public Triangle(int side1, int side2, int side3){
+    public void setSide1(int side1) {
+        this.side1 = side1;
+    }
+
+    public int getSide2() {
+        return side2;
+    }
+
+    public void setSide2(int side2) {
+        this.side2 = side2;
+    }
+
+    public int getSide3() {
+        return side3;
+    }
+
+    public void setSide3(int side3) {
+        this.side3 = side3;
+    }
+
+    public Triangle() {
+    }
+
+    public Triangle(int side1, int side2, int side3) {
         this.side1 = side1;
         this.side2 = side2;
         this.side3 = side3;
+    }
+
+
+    public double getSquare() {
+        double d = (side1 + side2 + side3) / 2;
+        return Math.sqrt(d * (d - side1) * (d - side3) * (d - side2));
     }
 
     @Override
@@ -21,8 +52,5 @@ public class Triangle {
                 ", side3=" + side3 +
                 '}';
     }
-    public double getSquare() {
-        double d = (side1 + side2 + side3) / 2;
-        return Math.sqrt(d * (d - side1) * (d - side3) * (d - side2));
-    }
+
 }
