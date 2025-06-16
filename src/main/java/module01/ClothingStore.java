fc047fe104668d63572b8a3fc7b5cf788b0f802e
Mitina Daryna <mitinadari@gmail.com>
2025-06-17 02:03:24 +0300

    test

--- src/main/java/module01/ClothingStore.java
+++ src/main/java/module01/ClothingStore.java
@@ -5,7 +5,7 @@ public class ClothingStore {
         String customerName = "Olena";
         int itemsBought = 2;
         double itemPrice = 750.50;
-        String paymentMethod ="cash";
+        String paymentMethod = "Cash";
         double totalCost = itemsBought*itemPrice;
         System.out.println("Welcome, " + customerName +", thank you for your purchase!");
         System.out.println("Number ot items: " + itemsBought);
