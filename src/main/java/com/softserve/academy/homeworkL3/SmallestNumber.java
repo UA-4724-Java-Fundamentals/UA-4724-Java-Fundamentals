69d759cad8955a10d4c87b6dab8c0974f3eadb97
Vlada <vladaaj9@gmail.com>
2025-06-20 22:23:27 +0200

    module01 added

--- src/main/java/com/softserve/academy/homeworkL3/SmallestNumber.java
+++ src/main/java/com/softserve/academy/homeworkL3/SmallestNumber.java
@@ -1,32 +1,32 @@
 package com.softserve.academy.homeworkL3;
-//
+
 import java.util.Scanner;
 
-import static java.lang.System.in;
-
 public class SmallestNumber {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(in);
+    public static int findSmallest(int a, int b, int c) {
+        int smallest = a;
 
-        int first1 = getSmall("Input the first number: ");
-        int second2 = getSmall("Input the second number: ");
-        int third3 = getSmall("Input the third number: ");
+        if (b < smallest) {
+            smallest = b;
+        }
+        if (c < smallest) {
+            smallest = c;
+        }
+        return smallest;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Input the first number: ");
+        int first1 = scanner.nextInt();
+
+        System.out.println("Input the second number: ");
+        int second2 = scanner.nextInt();
+        System.out.println("Input the third number: ");
+        int third3 = scanner.nextInt();
 
         int smallest = findSmallest(first1, second2, third3);
-
         System.out.println("Smallest number: " + smallest);
     }
-    private static int getSmall(String s) {
-        Scanner scanner = new Scanner(in);
-        System.out.println();
-    }
-
-    public static int findSmallest(int a, int b, int c) {
-        int min = a;
-
-
-        return min;
-    }
-
-
 }
