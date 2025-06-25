e8bf43195761e5c94927748377118d49b0d33f3f
Yaroslav <garasym@gmail.com>
2025-06-25 18:49:58 +0300

    AppPrime. Fixed 2.

--- src/main/java/com/softserve/academy/module05tst/AppPrime.java
+++ src/main/java/com/softserve/academy/module05tst/AppPrime.java
@@ -8,15 +8,18 @@ public class AppPrime {
         Scanner scanner = new Scanner(System.in);
         //
         System.out.print("input k = ");
-        int k = scanner.nextInt();
+        int k = Math.abs(scanner.nextInt());
         //
-        boolean isPrime = true;
-        for (int i = 3; i <= (int) (Math.sqrt(k) + 0.1); i = i + 2) {
-            System.out.print(i + "  ");
-            if (k % i == 0) {
-                System.out.print(k + " % " + i + " == 0");
-                isPrime = false;
-                break;
+        boolean isPrime = false;
+        if ((k != 0) && (k != 1)) {
+            isPrime = true;
+            for (int i = 3; i <= (int) (Math.sqrt(k) + 0.1); i = i + 2) {
+                System.out.print(i + "  ");
+                if (k % i == 0) {
+                    System.out.print(k + " % " + i + " == 0");
+                    isPrime = false;
+                    break;
+                }
             }
         }
         System.out.print("\nisPrime = " + isPrime);
