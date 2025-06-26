dd1fedd58fd2495538f6427bfd2eeb54ceb88cf7
Oleksandr Kozynets <kozynetsoleksandr8@gmail.com>
2025-06-26 15:25:05 +0100

    update homework06 Developer and Employee classes

--- src/main/java/com/softserve/academy/homework06/employee/Developer.java
+++ src/main/java/com/softserve/academy/homework06/employee/Developer.java
@@ -1,15 +1,30 @@
 package com.softserve.academy.homework06.employee;
 
 public class Developer extends Employee {
-    private final String position;
+    private String position;
 
     public Developer(String name, int age, double salary, String position) {
         super(name, age, salary);
         this.position = position;
     }
 
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
     @Override
     public String report() {
-        return String.format("Name: %s, Age: %d years, Position: %s, Salary: %.2f", this.name, this.age, this.position, this.salary);
+        return String.format("Name: %s, Age: %d years, Position: %s, Salary: %.2f", getName(), getAge(), getPosition(), getSalary());
+    }
+
+    @Override
+    public String toString() {
+        return "Developer{" +
+                "position='" + position + '\'' +
+                '}';
     }
 }
--- src/main/java/com/softserve/academy/homework06/employee/Employee.java
+++ src/main/java/com/softserve/academy/homework06/employee/Employee.java
@@ -1,9 +1,9 @@
 package com.softserve.academy.homework06.employee;
 
 public class Employee {
-    String name;
-    int age;
-    double salary;
+    private String name;
+    private int age;
+    private double salary;
 
     public Employee(String name, int age, double salary) {
         this.name = name;
@@ -11,7 +11,40 @@ public class Employee {
         this.salary = salary;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
     public String report() {
         return String.format("Name: %s, Age: %d, Salary: ₴ %.2f", name, age, salary);
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", salary=" + salary +
+                '}';
     }
 }
