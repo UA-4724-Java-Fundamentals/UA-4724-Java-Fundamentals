55343677dfda6c713901f6f176b5b038a24d12f2
Oleksandr Kozynets <kozynetsoleksandr8@gmail.com>
2025-07-08 18:43:28 +0100

    add codeWars for homework09

--- src/main/java/com/softserve/academy/homework09/codewars/EncapsulationDemo.java
+++ src/main/java/com/softserve/academy/homework09/codewars/EncapsulationDemo.java
@@ -5,11 +5,23 @@ public class EncapsulationDemo {
     private String stringValue;
     private Object anObject;
 
+    public EncapsulationDemo() {
+    }
+
+    public EncapsulationDemo(int number, String stringValue, Object anObject) {
+        this.number = number;
+        this.stringValue = stringValue;
+        this.anObject = anObject;
+    }
+
     public int getNumber() {
         return number;
     }
 
     public void setNumber(int number) {
+        if (number < 0) {
+            throw new IllegalArgumentException();
+        }
         this.number = number;
     }
 
@@ -18,6 +30,9 @@ public class EncapsulationDemo {
     }
 
     public void setStringValue(String stringValue) {
+        if (stringValue == null) {
+            throw new IllegalArgumentException("stringValue is null");
+        }
         this.stringValue = stringValue;
     }
 
@@ -26,15 +41,18 @@ public class EncapsulationDemo {
     }
 
     public void setAnObject(Object anObject) {
+        if (anObject == null) {
+            throw new IllegalArgumentException("An object is null");
+        }
         this.anObject = anObject;
     }
 
-    public EncapsulationDemo() {
-    }
-
-    public EncapsulationDemo(int number, String stringValue, Object anObject) {
-        this.number = number;
-        this.stringValue = stringValue;
-        this.anObject = anObject;
+    @Override
+    public String toString() {
+        return "EncapsulationDemo{" +
+                "number=" + number +
+                ", stringValue='" + stringValue + '\'' +
+                ", anObject=" + anObject +
+                '}';
     }
 }
