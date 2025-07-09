251a4661c33d479513f3fac7a9475120f18605b9
Yaroslav <garasym@gmail.com>
2025-07-09 18:37:05 +0300

    Wrapper. Generic

---
+++ src/main/java/com/softserve/academy/module10box/Box.java
@@ -0,0 +1,14 @@
+package com.softserve.academy.module10box;
+
+public class Box {
+	private Object obj;
+
+	public Object get() {
+		return obj;
+	}
+
+	public void set(Object obj) {
+		this.obj = obj;
+	}
+
+}         
