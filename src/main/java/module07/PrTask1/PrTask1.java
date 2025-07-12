bb860cc5f2278bbef182a8a34a7394ce85bcc7a0
Ihor <ozymandias.0413@gmail.com>
2025-07-12 21:12:17 +0300

    module007.PrTask

---
+++ src/main/java/module07/PrTask1/PrTask1.java
@@ -0,0 +1,43 @@
+package module07.PrTask1;
+
+public class PrTask1 {
+    public static void main(String[] args) {
+        Animal[] animals = {
+                new Cat(),
+                new Dog()
+        };
+        for (Animal animal : animals) {
+            animal.voice();
+            animal.feed();
+        }
+    }
+}
+
+interface Animal{
+    void voice();
+    void feed();
+}
+
+class Cat implements Animal{
+    @Override
+    public void voice() {
+        System.out.println("Meow");
+    }
+    @Override
+    public void feed() {
+        System.out.println("The cat eats its jelly food");
+    }
+}
+
+class Dog implements Animal{
+    @Override
+    public void voice() {
+        System.out.println("Woof");
+    }
+
+    @Override
+    public void feed() {
+        System.out.println("The dog eats some tasty bone");
+
+    }
+}
---
+++ src/main/java/module07/PrTask2/PrTask2.java
@@ -0,0 +1,93 @@
+package module07.PrTask2;
+
+public class PrTask2 {
+    public static void main(String[] args) {
+        Person[] persons = {
+                new Teacher("John"),
+                new Cleaner("Bob"),
+                new Student("Alice")
+        };
+        for( var person : persons ){
+            person.print();
+            if( person instanceof Staff staff ){
+                System.out.println("My salary is " + staff.salary());
+            }
+        }
+    }
+}
+
+abstract class Person {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+    public Person(String name) {
+        this.name = name;
+    }
+
+   public  void print(){
+        System.out.println("My name is " + name);
+   }
+}
+
+abstract class Staff extends Person {
+    abstract int salary();
+
+    public Staff(String name) {
+        super(name);
+    }
+}
+
+class Teacher extends Staff {
+    private static final String TYPE_PERSON = "Teacher";
+
+    public Teacher(String name) {
+        super(name);
+    }
+
+    @Override
+    int salary() {
+        return 10_000;
+    }
+
+    @Override
+   public void print() {
+        super.print();
+        System.out.println("I am a " + TYPE_PERSON);
+    }
+}
+
+class Cleaner extends Staff {
+
+    private static final String TYPE_PERSON = "Cleaner";
+
+    public Cleaner(String name) {
+        super(name);
+    }
+
+    @Override
+    int salary() {
+        return 5_000;
+    }
+
+    @Override
+    public void print() {
+        super.print();
+        System.out.println("I am a " + TYPE_PERSON);
+    }
+}
+
+class Student extends Person {
+    private static final String TYPE_PERSON = "Student";
+
+    public Student(String name) {
+        super(name);
+    }
+
+    @Override
+    public void print() {
+        super.print();
+        System.out.println("I am a " + TYPE_PERSON);
+    }
+}
