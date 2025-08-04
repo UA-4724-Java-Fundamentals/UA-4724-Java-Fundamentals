30ed96eb6b98c190c4a1d1ebae58229f65d66b3b
Yaroslav <garasym@gmail.com>
2025-08-04 19:57:32 +0300

    Server. Client

--- src/main/java/com/softserve/academy/module16file/PathUri.java
+++ src/main/java/com/softserve/academy/module16file/PathUri.java
@@ -3,12 +3,16 @@ package com.softserve.academy.module16file;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.net.URL;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class PathUri {
     public static void main(String[] args) throws IOException, URISyntaxException {
         String uribase = "https://www.example.com/path/to/resource";
+//        URL url = new URL(uribase);
+//        System.out.println("url = " + url);
+        //
         URI uri = new URI(uribase);
         System.out.println("uri = " + uri);
         //
